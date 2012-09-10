97a7f40f12f5e443a5809556760d03c5df4e8042
nathanmaton <nate.maton@gmail.com>
2012-09-10 10:23:23 -0400

    final mods before db seeds test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+portfoliof1 = Blog.create(filename: 'nathan_maton_4see.png', caption: '', isfeatured: , subcaption = '', full_img1 = '', full_img2 = '', full_title = '', full_body = '')
+portfoliof2 = Blog.find(2)
+portfoliof3 = Blog.find(3)
+portfolio1 = Blog.find(4)
+portfolio2 = Blog.find(5)
+portfolio3 = Blog.find(6)
