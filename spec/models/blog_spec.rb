51cae7a559b1080d617c093e9a968ad00930d68c
nathanmaton <nate.maton@gmail.com>
2012-09-10 17:23:44 -0400

    home page styled.

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -7,6 +7,12 @@
 #  caption    :string(255)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  isfeatured :boolean
+#  subcaption :string(255)
+#  full_img1  :string(255)
+#  full_img2  :string(255)
+#  full_title :string(255)
+#  full_body  :string(255)
 #
 
 require 'spec_helper'
