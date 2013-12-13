72d1d81ee4bc646a22cbd92acf254d424a649bbe
Nathan Maton <nathanmaton@gmail.com>
2013-12-13 15:19:07 -0800

    publications and blog pages

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Matonportfolio::Application.routes.draw do
 
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
+  match '/publications', to: 'static_pages#publications'
+
   match '/contact', to: 'static_pages#contact'
 
   #resources :blog 
